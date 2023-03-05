4f34b4f5fecf1b893439628b5470ab96c0e5e6ca
Oguzhan Demiraslan <oguzhandemiraslan@gmail.com>
2023-03-05 02:26:57 -0500

    add error handling

---
+++ src/app/search/error.js
@@ -0,0 +1,18 @@
+"use client";
+
+import { useEffect } from "react";
+
+function Error({ error, reset }) {
+  useEffect(() => {}, [error]);
+
+  return (
+    <div className="flex flex-col justify-center items-center py-10">
+      <h1 className="text-3xl mb-4">Something went wrong!</h1>
+      <button className="text-blue-500" onClick={() => reset()}>
+        Try Again
+      </button>
+    </div>
+  );
+}
+
+export default Error;
--- src/app/search/web/page.js
+++ src/app/search/web/page.js
@@ -1,10 +1,30 @@
+import Link from "next/link";
+
 async function WebSearchPage({ searchParams }) {
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
 
+  if (!response.ok) {
+    throw new Error("Something went wrong!");
+  }
+
   const data = await response.json();
   const results = data.items;
+
+  if (!results) {
+    return (
+      <div className="flex flex-col items-center justify-center pt-10">
+        <h1 className="text-3xl mb-4">No results found</h1>
+        <p className="text-lg">
+          Try search something else or go back to {""}
+          <Link href="/" className="text-blue-500">
+            homepage.
+          </Link>
+        </p>
+      </div>
+    );
+  }
 
   return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
 }
