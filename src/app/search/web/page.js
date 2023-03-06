792102fa53ace850dcc781af44cdc19812c9e59a
Oguzhan Demiraslan <oguzhandemiraslan@gmail.com>
2023-03-06 13:36:34 -0500

    add search results component

--- src/app/search/web/page.js
+++ src/app/search/web/page.js
@@ -1,3 +1,4 @@
+import WebSearchResults from "@/components/WebSearchResults";
 import Link from "next/link";
 
 async function WebSearchPage({ searchParams }) {
@@ -26,7 +27,7 @@ async function WebSearchPage({ searchParams }) {
     );
   }
 
-  return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
+  return <>{results && <WebSearchResults results={data} />}</>;
 }
 
 export default WebSearchPage;
